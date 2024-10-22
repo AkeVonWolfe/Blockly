d9760f097ef2cfc4c0d7dfa3509292bf30dfdab3
AkeVonWolfe <94.andreas.svensson@gmail.com>
2024-10-22 15:16:41 +0200

    blockly

---
+++ blockly.js
@@ -0,0 +1,21 @@
+var Variable1, Variable2, Variable3, Variable4;
+
+
+console.log('andreas')
+console.log(1024)
+console.log(11 + 5)
+console.log(4 * 64)
+console.log(4 - 64)
+console.log(1 + (100 - 50))
+console.log(1 + 2 + 3 + 4)
+Variable1 = 16;
+console.log(Variable1)
+Variable2 = 2 * 4;
+console.log(Variable2)
+Variable3 = 10;
+Variable3 = 100;
+Variable3 = 50;
+console.log(Variable3)
+Variable4 = 10;
+Variable4 = Variable4 == 1;
+console.log(Variable4)
