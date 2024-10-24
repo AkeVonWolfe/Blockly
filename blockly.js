1c9e398b6ed3205c1c4dd78dd9b6a2df5d6d2686
Akevonwolfe <94.andreas.svensson@gmail.com>
2024-10-24 13:33:33 +0200

    blockey

--- blockly.js
+++ blockly.js
@@ -1,4 +1,5 @@
 var Variable1, Variable2, Variable3, Variable4, x, y;
+var variable7, variable8, variable9a, variable9b, variable9c, variable10, Variable12, month, variable14, variable15, variable16a, variable16b;
 
 
 console.log('andreas')
@@ -25,3 +26,48 @@ x = y + 1;
 y = x + 1;
 console.log(x)
 console.log(y)
+
+variable7 = 20;
+variable7 = variable7 + 1;
+console.log(variable7)
+variable8 = 500;
+variable8 = variable8 - 100;
+console.log(variable8)
+variable9a = 3;
+variable9b = 4;
+variable9c = 5;
+console.log(variable9a + variable9b + variable9c)
+console.log(variable9a * variable9b * variable9c)
+variable10 = true;
+if (false) {
+  console.log('Villkoret är sant')
+} else {
+  console.log('Villkoret är falsk')
+}
+Variable12 = 145;
+if (Variable12 >= 130) {
+  console.log('Du får åka Balder!')
+}
+month = 10;
+if (month >= 9 && month == 11) {
+  console.log('month')
+}
+variable14 = 3;
+variable14 = variable14 * 5;
+if (variable14 == 10) {
+  console.log('stort')
+} else {
+  console.log('litet')
+}
+variable15 = 1;
+while (variable15 <= 3) {
+  variable15 = variable15 + 1;
+  console.log(variable15)
+}
+variable16a = 1;
+variable16b = 2;
+while (variable16a < 10) {
+  variable16a = variable16a + 1;
+  variable16b = variable16b * 2;
+}
+console.log(variable16b)
